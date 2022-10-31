4a25ff266c519cb2b9cfe234adadd665221130da
Raman Belaruski <ramanbelaruski@Ramans-MacBook-Air.local>
2022-10-31 08:58:11 +0100

    integrated cart service

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -3,5 +3,5 @@ export const API_PATHS = {
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	import: 'https://aajmsofw87.execute-api.eu-west-1.amazonaws.com/dev',
 	bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+	cart: 'https://jud941fsga.execute-api.eu-west-1.amazonaws.com/dev',
 };
